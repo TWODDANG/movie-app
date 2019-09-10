acd3cf1187509ab7ea9ce0d1b7b1fb67838b53ff
2dd <79iug12@gmail.com>
2019-09-11 00:56:20 +0900

    Animation Added

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
 import {movies, tv} from "../../api";
+import {Animated} from "react-native";
 
 export default class extends React.Component {
     state = {
@@ -8,6 +9,9 @@ export default class extends React.Component {
         movieResults: null,
         tvResults: null,
         searchTerm: "",
+        fade: new Animated.Value(1),
+        fade2: new Animated.Value(0),
+        fade3: new Animated.Value(0),
         error: null,
     };
 
@@ -16,6 +20,52 @@ export default class extends React.Component {
             searchTerm: text
         });
     };
+
+    componentWillMount() {
+
+       Animated.sequence([
+           Animated.timing(
+               this.state.fade2,
+               {
+                   toValue: 1,
+                   duration: 900,
+                   useNativeDriver: true
+               }
+           ),
+           Animated.timing(
+               this.state.fade3,
+               {
+                   toValue: 1,
+                   duration: 900,
+                   useNativeDriver: true
+               }
+           ),
+           Animated.timing(
+               this.state.fade,
+               {
+                   toValue: 0,
+                   duration: 2000,
+                   useNativeDriver: true
+               }
+           ),
+           Animated.timing(
+               this.state.fade2,
+               {
+                   toValue: 0,
+                   duration: 3000,
+                   useNativeDriver: true
+               }
+           ),
+           Animated.timing(
+               this.state.fade3,
+               {
+                   toValue: 0,
+                   duration: 3000,
+                   useNativeDriver: true
+               }
+           )
+       ]).start();
+    }
 
     onSubmitEditing = async() => {
         const {searchTerm} = this.state;
@@ -47,7 +97,7 @@ export default class extends React.Component {
     };
 
     render() {
-        const { loading, movieResults, tvResults, searchTerm } = this.state;
+        const { loading, movieResults, tvResults, searchTerm, fade, fade2, fade3 } = this.state;
         return (
             <SearchPresenter
                 loading={loading}
@@ -56,6 +106,9 @@ export default class extends React.Component {
                 searchTerm={searchTerm}
                 handleSearchUpdate={this.handleSearchUpdate}
                 onSubmitEditing={this.onSubmitEditing}
+                fade={fade}
+                fade2={fade2}
+                fade3={fade3}
             />
         );
     }
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -6,6 +6,8 @@ import Layout from "../../constants/Layout";
 import Loader from "../../components/Loader";
 import Section from "../../components/Section";
 import MovieItem from "../../components/MovieItem";
+import {Animated, Linking} from 'react-native';
+
 
 
 const Container = styled.View`
@@ -50,68 +52,93 @@ const SearchPresenter = ({
                              searchTerm,
                              movieResults,
                              handleSearchUpdate,
-                             onSubmitEditing
-                         }) =>(
-         <Container>
-        <InputContainer>
-        <Input
-            onChangeText={handleSearchUpdate}
-            value={searchTerm}
-            returnKeyType={"search"}
-            placeholder = 'Search 2dd/영어 검색만 가능'
-            placeholderTextColor={GREY_COLOR}
-            onSubmitEditing={onSubmitEditing}
-        />
-        </InputContainer>
-        <SearchResults>
-            {loading ? <Loader/> : (
-            <>
-                {movieResults ? (
-                    movieResults.length > 0 ? (
-                        <Section title={'Movie Results'}>
-                            {movieResults
-                                .filter(movie=>movie.poster_path !== null)
-                                .map(movie => <MovieItem
-                                    key={movie.id}
-                                    posterPhoto={movie.poster_path}
-                                    voteAvg={movie.vote_average}
-                                    id={movie.id}
-                                    title={movie.title}
-                                />)}
-                        </Section>
-                    ) : <TextView><Text>영화가 없어요:)</Text></TextView>
-                ) : <Text>이댕댕 무비 서처:)</Text> }
-                {tvResults ? (
-                    tvResults.length > 0 ? (
-                        <Section title="TV Results">
-                            {tvResults
-                                .filter(tv => tv.poster_path !== null)
-                                .map(tv => (
-                                    <MovieItem
-                                        isMovie={false}
-                                        key={tv.id}
-                                        id={tv.id}
-                                        posterPhoto={tv.poster_path}
-                                        title={tv.name}
-                                        voteAvg={tv.vote_average}
-                                    />
-                                ))}
-                        </Section>
-                    ) : (<TextView><Text>티비 프로그램이 없어요:)</Text></TextView>)
-                ) : (<TextView>
-                       <Text>hi~</Text>
-                </TextView>
-                )
-                }
-                {tvResults ? null :
-                    <Text>{`Visit => http://2dd.kr`}</Text>
-                }
+                             onSubmitEditing,
+                             fade,
+                             fade2,
+                             fade3
 
-            </>
-        ) }
-        </SearchResults>
-    </Container>
-);
+                         }) => {
+
+  return (<Container>
+      <InputContainer>
+          <Input
+              onChangeText={handleSearchUpdate}
+              value={searchTerm}
+              returnKeyType={"search"}
+              placeholder = 'Search 2dd/영어 검색만 가능'
+              placeholderTextColor={GREY_COLOR}
+              onSubmitEditing={onSubmitEditing}
+          />
+      </InputContainer>
+      <SearchResults>
+          {loading ? <Loader/> : (
+              <>
+                  {movieResults ? (
+                      movieResults.length > 0 ? (
+                          <Section title={'Movie Results'}>
+                              {movieResults
+                                  .filter(movie=>movie.poster_path !== null)
+                                  .map(movie => <MovieItem
+                                      key={movie.id}
+                                      posterPhoto={movie.poster_path}
+                                      voteAvg={movie.vote_average}
+                                      id={movie.id}
+                                      title={movie.title}
+                                  />)}
+                          </Section>
+                      ) : <TextView><Text>영화가 없어요:)</Text></TextView>
+                  ) : <Animated.View style={{opacity :fade}}>
+                      <TextView>
+                          <Text>이댕댕 무비 서처 :)</Text>
+                      </TextView>
+                      </Animated.View>
+
+                   }
+                  {tvResults ? (
+                      tvResults.length > 0 ? (
+                          <Section title="TV Results">
+                              {tvResults
+                                  .filter(tv => tv.poster_path !== null)
+                                  .map(tv => (
+                                      <MovieItem
+                                          isMovie={false}
+                                          key={tv.id}
+                                          id={tv.id}
+                                          posterPhoto={tv.poster_path}
+                                          title={tv.name}
+                                          voteAvg={tv.vote_average}
+                                      />
+                                  ))}
+                          </Section>
+                      ) : (<TextView><Text>티비 프로그램이 없어요:)</Text></TextView>)
+                  ) : (<Animated.View style={{opacity :fade2}}>
+                          <TextView>
+                              <Text>{`\n이댕댕 스튜디오 RN 프로젝트: \n 19년 7월~19년 9월\n`} </Text>
+                          </TextView>
+
+                      </Animated.View>
+                  )
+                  }
+                  {tvResults ? null : (
+                      <Animated.View style={{opacity :fade3}}>
+                          <TextView>
+                              <Text style={{color: '#00ffff'}}
+                                    onPress={() => Linking.openURL('http://2dd.kr')}>
+                                  http://2dd.kr
+                              </Text>
+                              <Text>
+                                  위 링크를 눌러 개인 홈페이지를 방문해보세요! @-@
+                              </Text>
+                          </TextView>
+                      </Animated.View>
+                  )
+                  }
+
+              </>
+          ) }
+      </SearchResults>
+  </Container>)
+};
 
 SearchPresenter.propTypes = {
     loading: PropTypes.bool.isRequired,
